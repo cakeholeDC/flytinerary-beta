0cb9ba64d60a24c696cab63f1b87f3732db8f4f2
Kyle <kylepcole@gmail.com>
2019-12-27 18:25:52 -0500

    form dropdowns now functional

--- flytinerary-front-end/src/trip_show_page.js
+++ flytinerary-front-end/src/trip_show_page.js
@@ -12,7 +12,7 @@ function getTripData(trip_id) {
 
 function buildAgendaTimeline(trip) {
 	const agenda = createWithClasses('div','list')
-	agenda.id = "trip-agenda"
+	agenda.id = `trip-${trip.id}-agenda`
 
 	let agendaDate = '' // determines date uniqueness
 	let eventsContainer = '' // assigns the UL for each date
@@ -64,7 +64,7 @@ function loadAgendaPage(trip) {
 	siteHeader.innerText = trip.nickname
 
 	const subHeader = document.querySelector('#site-sub-header')
-	subHeader.innerText = `${trip.destination} / ${formatDate(trip.start_date)} — ${formatDate(trip.end_date)}`
+	subHeader.innerText = `${trip.destination}: ${formatDate(trip.start_date)} — ${formatDate(trip.end_date)}`
 
 	document.querySelector('#search-container').innerHTML = `<i class="plane icon"></i>
 				${trip.attendees.length} Travellers`
@@ -73,8 +73,9 @@ function loadAgendaPage(trip) {
 	headerContainer.innerText = "Itinerary"
 	headerContainer.id = 'trip-agenda-header'
 
-	const createEventBtn = createWithClasses('button','ui','button','right','float')
+	const createEventBtn = createWithClasses('button','ui','button','right','floated')
 	createEventBtn.innerText = "Add Event"
+	createEventBtn.id = "new-event-button"
 	createEventBtn.addEventListener('click', buildNewEventForm)
 	headerContainer.appendChild(createEventBtn)
 	
@@ -84,49 +85,99 @@ function loadAgendaPage(trip) {
 
 function buildNewEventForm(event) {
 	console.log("Adding Event")
+	document.querySelector("#new-event-button").style.display = "none"
 
-	let agenda = document.querySelector('#trip-agenda')
+	let agenda = document.querySelector('#main-column .list')
 
 	let form = createWithClasses('form', 'ui','form')
+		
+		const traveller = createFormInputLabel("Traveller", "text", "traveller_id", "Traveller Name")
+			traveller.id = "traveller-id"
+			traveller.querySelector('input').value = SESSION_USER
 
 		const eventType = createWithClasses('div','field')
 			const label = createWithClasses('label')
 			  label.innerText = "Event Type"
 			
-			let select = createWithClasses('select')
+			let select = createWithClasses('select', 'ui', 'form', 'selection', 'dropdown')
 			  select.name = "event_type"
-			  select.placeholder = "Event Type"
+			  // select.placeholder = "Event Type"
 			  select.innerHTML = `
-			  	<option name="flight">Flight</option>
-				<option name="food">Food</option>
-				<option name="lodging">Lodging</option>
-				<option name="activity">Activity</option>
+			  		  <option value="" disabled selected>Select Event Type</option>
+				      <option value="flight">Flight</option>
+				      <option value="food">Food</option>
+				      <option value="lodging">Lodging</option>
+				      <option value="activity">Activity</option>
+				      <option value="reservation">Reservation</option>
 			  `
 
 		eventType.append(label, select)
 			  
 		const dates = createWithClasses('div','field')
-			const dateLabel = document.createElement('label')
-			dateLabel.innerText = "Trip Dates"
-			dates.appendChild(dateLabel)
+			// const dateLabel = document.createElement('label', 'container')
+			// dateLabel.innerText = "Event Dates"
+			// dates.appendChild(dateLabel)
 
-			const today = new Date()
+			const tripDates = document.querySelector('#site-sub-header').innerText.split(':')
+
+			const tripStart = new Date(tripDates[1].split('—')[0].trim())
+			const tripEnd = new Date(tripDates[1].split('—')[1].trim())
+
 			const start_end = createWithClasses('div','two','fields')
-			  const start = createFormInputLabel('Depart', "date", "start", '', prefillDate(today))
-			  const end = createFormInputLabel('Return', "date", "end", '', prefillDate(today, true))
+			  const start = createFormInputLabel('Event Start', "date", "start", '', prefillDate(tripStart)) //@TODO rework function to accept string
+			  const end = createFormInputLabel('Event End', "date", "end", '', prefillDate(tripEnd))
 
 			start_end.append(start, end)
 		dates.appendChild(start_end)
 
-		const description = createFormInputLabel("Description", "text", "description", "Lorem ipsum dolor a mi...")
-		const traveller = createFormInputLabel("Traveller", "text", "traveller_id", "Traveller Name", "TEST CONTENT")
-			traveller.id = "traveller-id"
+		// THIS NEEDS A DIV FIELD!!!! 
+		// createWithClasses('div','field')
+		const description = createWithClasses('div', 'field')
+		  const descLabel = document.createElement('label')
+			descLabel.innerText = "Description"
+		  const descArea = document.createElement('textarea')
+			descArea.rows = 2
+			descArea.name = "description"
+			descArea.placeholder = "Lorem ipsum dolor a mi..."
+		description.append(descLabel, descArea)
 
-		let submit = createWithClasses('button', 'ui','button')
+		// const description = createFormInputLabel("Description", "text", "description", "Lorem ipsum dolor a mi...")
+			// description.innerHTML += '<textarea rows="2"></textarea>'
+
+
+		let submit = createWithClasses('button', 'ui','button', 'right','floated')
 		submit.innerText = "Create Event"
 
-	form.append(eventType, dates, description, traveller)
+	form.append(traveller, eventType, dates, description, submit)
+	form.addEventListener('submit', processNewEvent)
 
 	agenda.prepend(form)
+
+}
+
+function processNewEvent(event) {
+	event.preventDefault()
+	console.log('processNewEvent')
+	//@ TODO add JS form validator 
+
+	const agendaPage = document.querySelector('#main-column .list')
+	const tripID = Number(agendaPage.id.split('-')[1])
+
+	const newEventBody = {
+		event_type: event.target.event_type.selectedOptions[0].text,
+    	start: event.target.start.value,
+    	end: event.target.end.value,
+    	description: event.target.description.value,
+    	trip_id: tripID,
+    	// let user select another traveller
+    	// traveller_id: event.target.description.traveller_id,
+    	// OR
+    	// traveller is ALWAYS current user
+    	traveller_id: SESSION_USER
+	}
+
+	debugger
+
+	document.querySelector("#new-event-button").style.display = "inline-block"
 
 }
