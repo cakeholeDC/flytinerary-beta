619654f167c11e81bb1869a663541fb9b12e5f26
Kyle <kylepcole@gmail.com>
2020-01-02 09:03:53 -0500

    added readme and license

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# flytinerary
+# Flytinerary
+Flytinerary is a travel planning app created by [Kyle Cole](https://www.kylepcole.com/) as part of the [Flatiron School's Software Engineering Immersive Bootcamp](https://flatironschool.com/career-courses/coding-bootcamp)
+
+#Version
+Flytinerary v1.0.0
+
+## Instructions
+Use the field at the top of the page to start planning a trip. Once a trip has been created, you will be able to manage events for that trip.`
+
+#License
+MIT License
+
+Copyright (c) 2020 Kyle P Cole
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- flytinerary-front-end/src/main.js
+++ flytinerary-front-end/src/main.js
@@ -42,10 +42,34 @@ function loadAboutPage() {
 	aboutContent.innerHTML = `Flytinerary is a travel planning app created by <a target="_blank" href="https://www.kylepcole.com/">Kyle Cole</a> as part of the <a target="_blank" href="https://flatironschool.com/career-courses/coding-bootcamp">Flatiron School's Software Engineering Immersive Bootcamp</a>
 					<br><br>Use the field at the top of the page to start planning a trip. Once a trip has been created, you will be able to manage events for that trip.`
 
-	const applicationVersion = createWithClasses('div', 'center', 'aligned')
+
+	const license = createWithClasses('div', 'content', 'left', 'aligned')
+	license.innerText = `MIT License
+
+	Copyright (c) 2020 Kyle P Cole
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.`
+	
+	const applicationVersion = createWithClasses('p', 'center', 'aligned')
 	applicationVersion.innerText = 'Flytinerary v1.0.0'
 
-	content.append(aboutHeader, aboutContent, applicationVersion)
+	content.append(aboutHeader, aboutContent, applicationVersion, license)
 }
 
 function getPageBody() {
